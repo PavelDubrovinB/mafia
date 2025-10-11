fcf3e1f8cf6d127dea3599af7d8aed7abcbd6039
Pavel Dubrovin <p.dubrovin@velvetech.dev>
2025-10-11 13:31:23 +0500

    feat: update ui

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Сборка проекта
 yarn build
 
-# Копирование файлов в папку docs для GitHub Pages
 cp -r build/* docs/
-
-echo "Готово! Теперь можно коммитить и пушить в GitHub"
-echo "Сайт будет доступен по адресу: https://ваш-username.github.io/mafia2"
